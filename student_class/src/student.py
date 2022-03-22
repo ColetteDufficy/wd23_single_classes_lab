da4d02a22d958a3735f23bc0f3bb21d861523dcb
colette dufficy <c.dufficy@gmail.com>
2022-03-22 16:00:21 +0000

    final edits

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -2,12 +2,11 @@ class Student:
     def __init__(self, name, cohort):
         self.name = name
         self.cohort = cohort
-    
+        
     def talk(self):
-        return "I can talk!"
+       return "I can talk!"
     
     def say_favourite_language(self, language):
-        # return "I love " + language
+    #     # return "I love " + language
         return f"I love {language}"
     
-   
